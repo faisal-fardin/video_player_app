d84734e00785f9dd3d5f2959407666dffeea48e9
Faisal <faisalfardin77@gmail.com>
2024-01-02 12:10:52 +0600

    Edit something design

--- lib/custom_widget/custom_widget.dart
+++ lib/custom_widget/custom_widget.dart
@@ -16,31 +16,24 @@ class HeaderSection extends StatelessWidget {
                 'https://th.bing.com/th/id/R.86a8179b2fabd7783eeb7c5cbe5cfa03?rik=c4aPFE24miQgvQ&pid=ImgRaw&r=0'),
           ],
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
           child: ListTile(
-            title: const SizedBox(
+            title: SizedBox(
               width: 250,
               height: 30,
               child: Text(
                 'আরব নেতাদের যে ভুলে ফি*লি*স্তি*নের এই দুর্দাশা | আবু ত্বহা মুহাম্মদ আদনান',
                 style: TextStyle(
                   color: Color(0xFF1A202C),
-                  fontSize: 15,
+                  fontSize: 14,
                   fontFamily: 'Hind Siliguri',
                   fontWeight: FontWeight.w800,
                   height: 1,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.more_vert_outlined,
-                color: Color(0xFFD9D9D9),
-              ),
-            ),
-            subtitle: const Padding(
+            subtitle: Padding(
               padding: EdgeInsets.symmetric(vertical: 5.0),
               child: Row(
                 children: [
@@ -262,6 +255,124 @@ class SubscribeSection extends StatelessWidget {
             ),
           ),
         )
+      ],
+    );
+  }
+}
+
+class CommentSection extends StatelessWidget {
+  const CommentSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Comments   7.5K',
+              style: TextStyle(
+                color: Color(0xFF718096),
+                fontSize: 13,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.compress_rounded),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 70,
+          child: TextFormField(
+            keyboardType: TextInputType.text,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+            decoration: const InputDecoration(
+                suffixIcon: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 15.0, vertical: 10),
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 25,
+                    color: Colors.grey,
+                  ),
+                ),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Add Comment',
+                hintStyle: TextStyle(
+                    fontSize: 16, color: Color(0xFFC1BDB9)),
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color(0xFFE2E8F0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color(0xFFE2E8F0)),
+                )),
+          ),
+        ),
+        ListTile(
+          title: const Row(
+            children: [
+              Text(
+                'Fahmida khanom',
+                style: TextStyle(
+                  color: Color(0xFF718096),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                '2 days ago',
+                style: TextStyle(
+                  color: Color(0xFF718096),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          subtitle: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: SizedBox(
+              child: Text(
+                'হুজুরের বক্তব্য গুলো ইংরেজি তে অনুবাদ করে সারা পৃথিবীর মানুষদের কে শুনিয়ে দিতে হবে। কথা গুলো খুব দামি',
+                style: TextStyle(
+                  color: Color(0xFF4A5568),
+                  fontSize: 12,
+                  fontFamily: 'Hind Siliguri',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+          leading: Container(
+            width: 40,
+            height: 40,
+            decoration: const ShapeDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    "https://th.bing.com/th/id/OIP.1d7TQI67pwfr0F5jqTgD1AHaGw?rs=1&pid=ImgDetMain"),
+                fit: BoxFit.fill,
+              ),
+              shape: OvalBorder(),
+            ),
+          ),
+        ),
       ],
     );
   }
--- lib/pages/video_paly_screen.dart
+++ lib/pages/video_paly_screen.dart
@@ -12,125 +12,23 @@ class VideoPlayScreen extends StatefulWidget {
 class _VideoPlayScreenState extends State<VideoPlayScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
             children: [
-              const HeaderSection(),
+              HeaderSection(),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
                 child: Column(
                   children: [
-                    const ActionSection(),
-                    const SizedBox(
+                    ActionSection(),
+                    SizedBox(
                       height: 10,
                     ),
-                    const SubscribeSection(),
-                    const Divider(),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Comments   7.5K',
-                          style: TextStyle(
-                            color: Color(0xFF718096),
-                            fontSize: 13,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.compress_rounded),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 70,
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        style: const TextStyle(
-                          fontSize: 18,
-                        ),
-                        decoration: const InputDecoration(
-                            suffixIcon: Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 15.0, vertical: 10),
-                              child: Icon(
-                                Icons.arrow_forward_ios,
-                                size: 25,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            fillColor: Colors.white,
-                            filled: true,
-                            hintText: 'Add Comment',
-                            hintStyle: TextStyle(
-                                fontSize: 16, color: Color(0xFFC1BDB9)),
-                            border: OutlineInputBorder(),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  width: 2, color: Color(0xFFE2E8F0)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  width: 2, color: Color(0xFFE2E8F0)),
-                            )),
-                      ),
-                    ),
-                     ListTile(
-                      title: const Row(
-                        children: [
-                          Text(
-                            'Fahmida khanom',
-                            style: TextStyle(
-                              color: Color(0xFF718096),
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(width: 20,),
-                          Text(
-                            '2 days ago',
-                            style: TextStyle(
-                              color: Color(0xFF718096),
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-
-                        ],
-                      ),
-                      subtitle: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10.0),
-                        child: SizedBox(
-                          child: Text(
-                            'হুজুরের বক্তব্য গুলো ইংরেজি তে অনুবাদ করে সারা পৃথিবীর মানুষদের কে শুনিয়ে দিতে হবে। কথা গুলো খুব দামি',
-                            style: TextStyle(
-                              color: Color(0xFF4A5568),
-                              fontSize: 12,
-                              fontFamily: 'Hind Siliguri',
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ),
-                      leading: Container(
-                        width: 40,
-                        height: 40,
-                        decoration: const ShapeDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                "https://th.bing.com/th/id/OIP.1d7TQI67pwfr0F5jqTgD1AHaGw?rs=1&pid=ImgDetMain"),
-                            fit: BoxFit.fill,
-                          ),
-                          shape: OvalBorder(),
-                        ),
-                      ),
-                    )
+                    SubscribeSection(),
+                    Divider(),
+                    CommentSection(),
                   ],
                 ),
               ),
@@ -141,3 +39,5 @@ class _VideoPlayScreenState extends State<VideoPlayScreen> {
     );
   }
 }
+
+
